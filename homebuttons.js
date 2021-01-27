2ef3247387ec0bfd60cf210b6b812b0f73229a10
CryHavok01 <71348370+CryHavok01@users.noreply.github.com>
2021-01-27 17:14:06 -0500

    Create homebuttons.js

---
+++ homebuttons.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class HomeButtons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(e) {
+      const points = Number(e.target.value)
+      this.props.onClick(points);
+  }
+  
+  render() {
+      return(
+        <div className="home_buttons">
+            <button value='1' onClick={this.handleClick}>
+            Free Throw
+            </button>
+            <button value='2' onClick={this.handleClick}>
+            Field Goal
+            </button>
+            <button value='3' onClick={this.handleClick}>
+            Three Points
+            </button>
+        </div>
+    )
+  }
+}
